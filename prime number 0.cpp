ba06a4b55685fe331ec479d9733835e2c4278f60
satya surya sai pavan korubilli <saipavan5th@gmail.com>
2019-08-06 05:28:52 +0530

    Update prime number 0.cpp

--- prime number 0.cpp
+++ prime number 0.cpp
@@ -7,9 +7,9 @@ int main()
 	scanf("%d",&n);
 	for (i = 1;i <= n;i++) {
 	
-    	if (n % i == 0) {
+    	    if (n % i == 0) {
 	        count = count + 1;
-	    }
+	     }
 	}
 	if (count > 2) {
 		printf("not prime");
