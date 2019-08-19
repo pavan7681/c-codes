fa480dccab47bc01d1811b9c9b9e6b0635a30927
satya surya sai pavan korubilli <saipavan5th@gmail.com>
2019-08-19 21:27:35 +0530

    Update digits sum.cpp

--- digits sum.cpp
+++ digits sum.cpp
@@ -1,14 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	int i = 0,sum = 0,n,r,num[100];
+	int sum = 0,n;
 	printf("enter the number:");
 	scanf("%d",&n);
 	while (n != 0) {
-		   r = n % 10;
-		   num[i] = r;
+		   sum = sum + n % 10;
 		   n = n / 10;
-		   sum = sum + num[i];
 	}
 	printf("\nsum : %d",sum);
 }
